747d90f1e68cb059583e09fbbd23a9778a379f81
kalkidan12 <kalget12@gmail.com>
2023-01-21 20:25:25 +0300

    run  code func

--- script.py
+++ script.py
@@ -33,4 +33,19 @@ def save_as():
         set_file_path(path)
 
 
+def run():
+    if file_path == '':
+        save_prompt = Toplevel()
+        text = Label(save_prompt, text='Please save your code')
+        text.pack()
+        return
+    command = f'python {file_path}'
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    code_output.insert('1.0', output)
+    code_output.insert('1.0',  error)
+
+
+menu_bar = Menu(compiler)
 
