9ce575c9086e339e7bf3a16e0f890541d4a8b337
kalkidan12 <kalget12@gmail.com>
2023-01-21 20:16:39 +0300

    open file

---
+++ script.py
@@ -0,0 +1,25 @@
+# importing the modules
+from tkinter import *
+from tkinter.filedialog import asksaveasfilename, askopenfilename
+import subprocess
+
+compiler = Tk()
+compiler.title('Simple Python IDE')
+file_path = ''
+
+
+def set_file_path(path):
+    global file_path
+    file_path = path
+
+
+def open_file():
+    path = askopenfilename(filetypes=[('Python Files', '*.py')])
+    with open(path, 'r') as file:
+        code = file.read()
+        editor.delete('1.0', END)
+        editor.insert('1.0', code)
+        set_file_path(path)
+
+
+
